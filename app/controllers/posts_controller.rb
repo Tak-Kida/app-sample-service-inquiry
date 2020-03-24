047c598f9fca7ced556ff92859341469472dd6de
Tak-Kida <takumakida0@gmail.com>
2020-03-24 19:46:09 +0900

    ログイン機能,を修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-  #before_action :set_current_user
-  #before_action :authenticate_user, {only:
-  # [:index, :index_today, :index_urgent, :solved, :unsolved, :destroy, :show, :edit, :update]}
+  before_action :set_current_user
+  before_action :authenticate_user!, {only:
+   [:index, :index_today, :index_urgent, :solved, :unsolved, :destroy, :show, :edit, :update]}
    
     def new
       @post = Post.new
